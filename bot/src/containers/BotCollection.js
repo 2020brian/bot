3c7a9c5745b4a7951e21903a640a780f0023fdfb
onjorobrian <onjorobrian1@gmail.com>
2023-08-31 04:39:24 +0300

    done

---
+++ bot/src/containers/BotCollection.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+function BotCollection() {
+  //your code here
+
+
+    return (
+      <div className="ui four column grid">
+        <div className="row">
+          {/*...and here..*/}
+          Collection of all bots
+        </div>
+      </div>
+    );
+  }
+
+
+export default BotCollection;
---
+++ bot/src/containers/BotsPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function BotsPage () {
+  //start here with your code for step one
+
+
+    return <div>{/* put your components here */}</div>;
+  }
+
+
+export default BotsPage;
